1c6f2dca1ce7ebcb503cab00b11bb2abecc4167d
81254807 <81254807@corp.root>
2020-01-27 13:58:02 +0800

    dont show main content if no conversation is selected

--- src/app/v2/chatboxx/components/main/main.component.ts
+++ src/app/v2/chatboxx/components/main/main.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ConversationInterface } from 'src/app/v2/shared/interfaces/conversation.interface';
+import { Subscription } from 'rxjs';
+import { ConversationService } from 'src/app/v2/shared/services/conversation.service';
+import { CONVERSATION_SELECT } from '../../store/conversation/conversation.action';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subs: Subscription[] = [];
+  selectedConversation: ConversationInterface;
+
+  constructor(
+    private conversationSV: ConversationService
+  ) { }
 
   ngOnInit() {
+    this.subs = [
+      this.watchConversationState()
+    ];
   }
 
+  private watchConversationState() {
+    return this.conversationSV.conversationState.subscribe( x => {
+      switch (x.action.name) {
+        case CONVERSATION_SELECT:
+          this.selectedConversation = x.conversation.selected;
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach( x => x.unsubscribe());
+  }
 }
